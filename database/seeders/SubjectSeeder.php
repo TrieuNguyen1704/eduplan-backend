cf1e3555040c654ed1e7092e1526996f95d08f04
TrieuNguyen1704 <Shellingofficial@gmail.com>
2026-07-08 20:39:37 +0700

    Update main

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -2,16 +2,49 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Subject;
 
 class SubjectSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Lấy ra user đầu tiên để gắn môn học vào
+        $user = User::where('email', 'shellingofficial@gmail.com')->first();
+        if (!$user) return;
+
+        Subject::updateOrCreate(
+            ['code' => 'PSY101', 'user_id' => $user->id],
+            ['name' => 'Tâm lý học đại cương', 'credits' => 3, 'status' => 'active']
+        );
+
+        Subject::updateOrCreate(
+            ['code' => 'ENG103', 'user_id' => $user->id],
+            ['name' => 'IELTS 3', 'credits' => 3, 'status' => 'active']
+        );
+
+        Subject::updateOrCreate(
+            ['code' => 'IT202', 'user_id' => $user->id],
+            ['name' => 'Cơ sở dữ liệu nâng cao', 'credits' => 4, 'status' => 'active']
+        );
+        //Nin
+         $user = User::where('email', 'nin@gmail.com')->first();
+        if (!$user) return;
+
+        Subject::updateOrCreate(
+            ['code' => 'PSY101', 'user_id' => $user->id],
+            ['name' => 'Tâm lý học đại cương', 'credits' => 3, 'status' => 'active']
+        );
+
+        Subject::updateOrCreate(
+            ['code' => 'ENG103', 'user_id' => $user->id],
+            ['name' => 'IELTS 3', 'credits' => 3, 'status' => 'active']
+        );
+
+        Subject::updateOrCreate(
+            ['code' => 'IT202', 'user_id' => $user->id],
+            ['name' => 'Cơ sở dữ liệu nâng cao', 'credits' => 4, 'status' => 'active']
+        );
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Tài khoản chính của anh
+        User::updateOrCreate(
+            ['email' => 'shellingofficial@gmail.com'],
+            ['name' => 'Anh Shelling', 'password' => Hash::make('123456')]
+        );
+
+        //Tài khoản test của Nin
+        User::updateOrCreate(
+            ['email' => 'nin@gmail.com'],
+            ['name' => 'Nguyễn Văn A', 'password' => Hash::make('123456')]
+        );
     }
 }
