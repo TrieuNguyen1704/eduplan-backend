046527ad3400d30fe50102b01bacc8ea96d457bc
Mona-Ha <hathininh23@gmail.com>
2026-07-08 12:57:25 +0700

    ninh_seed_du_lieu

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Tài khoản chính của anh
+        User::updateOrCreate(
+            ['email' => 'shellingofficial@gmail.com'],
+            ['name' => 'Anh Shelling', 'password' => Hash::make('123456')]
+        );
+
+        // Bạn có thể bảo bạn phụ trách data seed thêm hàng loạt tài khoản test ở dưới này
+        User::updateOrCreate(
+            ['email' => 'sinhvien1@gmail.com'],
+            ['name' => 'Nguyễn Văn A', 'password' => Hash::make('123456')]
+        );
+         User::updateOrCreate(
+            ['email' => 'nin@gmail.com'],
+            ['name' => 'nin', 'password' => Hash::make('123456')]
+        );
     }
 }
